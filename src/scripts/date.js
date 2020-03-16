add110e0157bb9b3e8c38f85db48b57de72e5242
Denis Olekh <denisolekh@gmail.com>
2020-03-16 17:31:05 +0200

    hw

---
+++ src/scripts/date.js
@@ -0,0 +1,104 @@
+import { generateNumbers } from './events.js';
+
+let today = new Date();
+let monthDay = new Date().getDate();
+let weekDay = new Date().getDay() - 1;
+const daysLine = document.querySelector('.week-line');
+const week = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+let firstDay = 0;
+
+
+export const getMonday = () => {
+    while (today.getDay() !== 1) {
+        today.setDate(today.getDate() - 1);
+    }
+};
+
+getMonday();
+
+/* Get Days */
+
+const getDays = () => {
+    const result = [];
+    generateNumbers(0, 6).map(day => {
+        const newDay = new Date(today);
+        newDay.setDate(newDay.getDate() + day);
+
+        result.push(`
+                <div class="box-day">
+                    <span class="box-day__week"
+                    data-day-number="${day+firstDay}">
+                        ${week[new Date(newDay).getDay()]}
+                    </span>
+                    <span class="box-day__month"
+                    data-date-number="${new Date(newDay).getDate()}">
+                    ${new Date(newDay).getDate()}</span>
+                </div>`);
+    });
+
+    return result.join('');
+};
+
+export const renderDays = () => {
+    daysLine.innerHTML = getDays();
+};
+
+/**** Mark today element ****/
+export const markToday = () => {
+    document.querySelector(`[data-day-number="${weekDay}"]`).classList.add('box-day__week__today');
+    document.querySelector(`[data-date-number="${monthDay}"]`).classList.add('box-day__month__today');
+};
+
+/* Get month */
+
+const monthElem = document.querySelector('.title');
+const month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+let popupMonth;
+
+export const setCurrMonth = () => {
+    const daysElem = document.querySelectorAll('.box-day');
+    const withoutFirst = [...daysElem].splice(1);
+    let checkMonthInWeek = false;
+
+    for (let arg of withoutFirst) {
+        if (arg.textContent == 1)
+            checkMonthInWeek = true;
+    }
+
+    let currMonth = month[new Date(today).getMonth()];
+    monthElem.textContent = `${currMonth} ${today.getFullYear()}`;
+    popupMonth = new Date(today).getMonth() + 1;
+};
+
+
+const rightBtn = document.querySelector('.btn-right');
+const getNextWeek = () => {
+    today.setDate(today.getDate() + 7);
+    firstDay += 7;
+    renderDays();
+    setCurrMonth();
+    markToday();
+};
+rightBtn.addEventListener('click', getNextWeek);
+
+const leftBtn = document.querySelector('.btn-left');
+const getPriviousWeek = () => {
+    today.setDate(today.getDate() - 7);
+    firstDay -= 7;
+    renderDays();
+    setCurrMonth();
+    markToday();
+};
+leftBtn.addEventListener('click', getPriviousWeek);
+
+/*  Today BTN */
+const todayBtn = document.querySelector('.today-btn');
+const getActualWeek = () => {
+    today = new Date();
+    firstDay = 0;
+    getMonday();
+    renderDays();
+    setCurrMonth();
+    markToday();
+};
+todayBtn.addEventListener('click', getActualWeek);
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,63 +1,10 @@
-import { renderTimeColumn, generateNumbers } from './events.js';
+import { getMonday, renderDays, markToday, setCurrMonth } from './date.js';
+import { renderTimeColumn } from './events.js';
 
 
 renderTimeColumn();
 
-let today = new Date();
-let weekDay = new Date().getDay() - 1;
-const daysLine = document.querySelector('.week-line');
-const week = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN'];
-const firstDay = 0;
-
-
-const getMonday = () => {
-    while (today.getDay() !== 1) {
-        today.setDate(today.getDate() - 1);
-    }
-};
-
 getMonday();
-
-const getDays = () => {
-    const result = [];
-    generateNumbers(0, 6).map(day => {
-        const newDay = new Date(today);
-        newDay.setDate(newDay.getDate() + day);
-
-        result.push(`
-                <div class="box-day">
-                    <span class="box-day__week"
-                    data-day-number="${day+firstDay}">
-                        ${week[new Date(newDay).getDay()]}
-                    </span>
-                    <span class="box-day__month"
-                    data-date-number="${day+firstDay}">
-                    ${new Date(newDay).getDate()}</span>
-                </div>`);
-    });
-
-    return result.join('');
-};
-
-const renderDays = () => {
-    daysLine.innerHTML = getDays();
-    document.querySelector(`[data-day-number="${weekDay}"]`).classList.add('box-day__week__today')
-    document.querySelector(`[data-date-number="${weekDay}"]`).classList.add('box-day__month__today')
-};
-
-const getWeekAfter = () => {
-    const days = document.querySelectorAll('.box-day__month');
-    for (let num of days) { num.innerHTML = Number(num.textContent) + 7 };
-};
-const getWeekBefore = () => {
-    const days = document.querySelectorAll('.box-day__month');
-    for (let num of days) { num.innerHTML = Number(num.textContent) - 7 };
-};
-
-
-const btnRight = document.querySelector('.btn-right');
-btnRight.addEventListener('click', getWeekAfter, false);
-const btnLeft = document.querySelector('.btn-left');
-btnLeft.addEventListener('click', getWeekBefore, false);
-
+setCurrMonth();
 renderDays();
+markToday();
